b23e1d6b720b7b6db4d25882d35368a4c76504df
Thanh Long <longnt@bralyvn.com>
2025-06-06 14:12:41 +0700

    init project

---
+++ AI_review/AI_review/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI_review
+//
+//  Created by Thanh Long on 6/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AI_review/AI_reviewTests/AI_reviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_reviewTests.swift
+//  AI_reviewTests
+//
+//  Created by Thanh Long on 6/6/25.
+//
+
+import Testing
+@testable import AI_review
+
+struct AI_reviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
